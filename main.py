5b16e99ad7c6e7e9161b3c8c47a129f2ecdbce39
Lucas H Polazzo <lucashpolazzo2013@gmail.com>
2019-04-02 16:17:46 -0300

    Venda funcionando com devolução de troco e atualização de estoque

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ qntProduct2 = 10
 qntProduct3 = 10
 qntProduct4 = 10
 qntProduct5 = 10
-selectedProduct = ""
-qntCentavos5 = 0
+nameSelectedProduct = ""
+qntCentavos05 = 0
 qntCentavos10 = 0
 qntCentavos25 = 0
 qntCentavos50 = 0
@@ -19,21 +19,40 @@ qntNota10 = 0
 qntNota20 = 0
 qntNota50 = 0
 qntNota100 = 0
-valorTotal = qntCentavos5 * 0.05 + qntCentavos10 * 0.1 + qntCentavos25 * 0.25 + qntCentavos50 * 0.50 + qntNota1 * 1 + qntNota2 * 2 + qntNota5 * 5 + qntNota10 * 10 + qntNota20 * 20 + qntNota50 * 50 + qntNota100 * 100
+notaInserida = 0.0
+valorTotalInserido = 0
+valorBebida = 0
+notaTroco = 0
+valorTroco = 0
+qntCentavosMaquina05 = 20
+qntCentavosMaquina10 = 20
+qntCentavosMaquina25 = 20
+qntCentavosMaquina50 = 20
+qntNotaMaquina1 = 10
+qntNotaMaquina2 = 10
+qntNotaMaquina5 = 0
+qntNotaMaquina10 = 0
+qntNotaMaquina20 = 0
+qntNotaMaquina50 = 0
+qntNotaMaquina100 = 0
+valorTotalMaquina = qntCentavosMaquina05 * 0.05 + qntCentavosMaquina10 * 0.1 + qntCentavosMaquina25 * 0.25 + \
+                    qntCentavosMaquina50 * 0.50 + qntNotaMaquina1 * 1 + qntNotaMaquina2 * 2 + qntNotaMaquina5 * 5 + \
+                    qntNota10 * 10 + qntNotaMaquina20 * 20 + qntNotaMaquina50 * 50 + qntNotaMaquina100 * 100
 
 ##### Início #####
 
 # Selecionar Bebida
 
 while isRunning:
+
     print("")
     print("-----------------------------------")
     print("Número|  Bebida          | Valor")
-    print("  1   |  Coca-Cola       | R$ 3,50")
-    print("  2   |  Fanta Uva       | R$ 4,00")
-    print("  3   |  Fanta Laranja   | R$ 3,00")
-    print("  4   |  Sprite          | R$ 4,50")
-    print("  5   |  Água mineral    | R$ 2,50")
+    print("  1   |  Coca-Cola       | R$ 3.50")
+    print("  2   |  Fanta Uva       | R$ 4.00")
+    print("  3   |  Fanta Laranja   | R$ 3.00")
+    print("  4   |  Sprite          | R$ 4.50")
+    print("  5   |  Água mineral    | R$ 2.50")
     print("-----------------------------------")
     print("")
     idProduct = input("Digite o número da bebida desejada: \n")
@@ -62,40 +81,239 @@ while isRunning:
         print()
         print("-----------------------------------")
         print("Nota R$ | Quantidade | R$ Total")
-        print("  0,05  |     {}     |    {:.2f}".format(qntCentavos5, qntCentavos5 * 0.05))
-        print("  0,10  |     {}     |    {:.2f}".format(qntCentavos10, qntCentavos10 * 0.1))
-        print("  0,25  |     {}     |    {:.2f}".format(qntCentavos25, qntCentavos25 * 0.25))
-        print("  0,50  |     {}     |    {:.2f}".format(qntCentavos50, qntCentavos50 * 0.50))
-        print("  1,00  |     {}     |    {:.2f}".format(qntNota1, qntNota1 * 1))
-        print("  2,00  |     {}     |    {:.2f}".format(qntNota2, qntNota2 * 2))
-        print("  5,00  |     {}     |    {:.2f}".format(qntNota5, qntNota5 * 5))
-        print("  10,00 |     {}     |    {:.2f}".format(qntNota10, qntNota10 * 10))
-        print("  20,00 |     {}     |    {:.2f}".format(qntNota20, qntNota20 * 20))
-        print("  50,00 |     {}     |    {:.2f}".format(qntNota50, qntNota50 * 50))
-        print("  100,00|     {}     |    {:.2f}".format(qntNota100, qntNota100 * 100))
+        print("  0,05  |     {}     |    {:.2f}".format(qntCentavosMaquina05, qntCentavosMaquina05 * 0.05))
+        print("  0,10  |     {}     |    {:.2f}".format(qntCentavosMaquina10, qntCentavosMaquina10 * 0.1))
+        print("  0,25  |     {}     |    {:.2f}".format(qntCentavosMaquina25, qntCentavosMaquina25 * 0.25))
+        print("  0,50  |     {}     |    {:.2f}".format(qntCentavosMaquina50, qntCentavosMaquina50 * 0.50))
+        print("  1,00  |     {}     |    {:.2f}".format(qntNotaMaquina1, qntNotaMaquina1 * 1))
+        print("  2,00  |     {}     |    {:.2f}".format(qntNotaMaquina2, qntNotaMaquina2 * 2))
+        print("  5,00  |     {}     |    {:.2f}".format(qntNotaMaquina5, qntNotaMaquina5 * 5))
+        print("  10,00 |     {}     |    {:.2f}".format(qntNotaMaquina10, qntNotaMaquina10 * 10))
+        print("  20,00 |     {}     |    {:.2f}".format(qntNotaMaquina20, qntNotaMaquina20 * 20))
+        print("  50,00 |     {}     |    {:.2f}".format(qntNotaMaquina50, qntNotaMaquina50 * 50))
+        print("  100,00|     {}     |    {:.2f}".format(qntNotaMaquina100, qntNotaMaquina100 * 100))
         print("-----------------------------------")
-        print("A máquina possui R$ {:.2f}".format(valorTotal))
+        print("A máquina possui R$ {:.2f}".format(valorTotalMaquina))
         print()
+
     elif 0 < idProduct <= 5:
-        # Converção de ID para String dos produtos
+        # Converção de ID para String dos produtos e Valores
         if idProduct == 1:
-            selectedProduct = "Coca-Cola"
+            nameSelectedProduct = "Coca-Cola"
+            valorBebida = 3.5
         elif idProduct == 2:
-            selectedProduct = "Fanta Uva"
+            nameSelectedProduct = "Fanta Uva"
+            valorBebida = 4
         elif idProduct == 3:
-            selectedProduct = "Fanta Laranja"
+            nameSelectedProduct = "Fanta Laranja"
+            valorBebida = 3
         elif idProduct == 4:
-            selectedProduct = "Sprite"
+            nameSelectedProduct = "Sprite"
+            valorBebida = 4.5
         elif idProduct == 5:
-            selectedProduct = "Água Mineral"
+            nameSelectedProduct = "Água Mineral"
+            valorBebida = 2.5
 
-        # Verificação bebida
-        print("Você selecionou a bebida de {}, está correto?: (S/N)".format(selectedProduct))
+            # Verificação se está correto
+        print("Você selecionou a bebida de {}, está correto?: (S/N)".format(nameSelectedProduct))
         simOuNao = input()
-
         if simOuNao == "s" or simOuNao == "S":
-            print("Beleza")
-            isRunning = False
+            # Creditar notas inseridas
+            while valorTotalInserido < valorBebida:
+
+                if valorTotalInserido > 0 : print(
+                    "Você possui R$ {} de créditos, faltam R$ {}"
+                        .format(valorTotalInserido, valorBebida - valorTotalInserido)
+                )
+                if valorTotalInserido == 0 : print("O valor total é R$ ", valorBebida)
+                notaInserida = float(input("Insira o valor da nota ou moeda colocada na máquina: \n"))
+
+                if notaInserida == 0.05:
+                    qntCentavos05+= 1
+                    valorTotalInserido += 0.05
+
+                elif notaInserida == 0.1:
+                    qntCentavos10 += 1
+                    valorTotalInserido += 0.1
+
+                elif notaInserida == 0.25:
+                    qntCentavos25 += 1
+                    valorTotalInserido += 0.25
+
+                elif notaInserida == 0.5:
+                    qntCentavos50 += 1
+                    valorTotalInserido += 0.5
+
+                elif notaInserida == 1:
+                    qntNota1 += 1
+                    valorTotalInserido += 1
+
+                elif notaInserida == 2:
+                    qntNota2 += 1
+                    valorTotalInserido += 2
+
+                elif notaInserida == 5:
+                    qntNota5 += 1
+                    valorTotalInserido += 5
+
+                elif notaInserida == 10:
+                    qntNota10 += 1
+                    valorTotalInserido += 10
+
+                elif notaInserida == 20:
+                    qntNota20 += 1
+                    valorTotalInserido += 20
+
+                elif notaInserida == 50:
+                    qntNota50 += 1
+                    valorTotalInserido += 50
+
+                elif notaInserida == 100:
+                    qntNota100 += 1
+                    valorTotalInserido += 100
+
+                else : print("\nVocê digitou o valor de uma nota inexistente, digite novamente: \n")
+
+            if valorTotalInserido >= valorBebida:
+                valorTroco = valorTotalInserido - valorBebida
+
+                # Devolver Troco
+                if valorTroco > 0:
+                    while valorTroco > 0:
+
+                        if valorTroco == 0.05 and qntCentavosMaquina05 > 0:
+                            notaTroco = 0.05
+                            valorTroco += -0.05
+                            qntCentavosMaquina05 += -1
+
+                        elif valorTroco == 0.1 and qntCentavosMaquina10 > 0:
+                            notaTroco = 0.1
+                            valorTroco += -0.1
+                            qntCentavosMaquina10 += -1
+
+                        elif valorTroco == 0.25 and qntCentavosMaquina25 > 0:
+                            notaTroco = 0.25
+                            valorTroco += -0.25
+                            qntCentavosMaquina25 += -1
+
+                        elif valorTroco == 0.5 and qntCentavosMaquina50 > 0:
+                            notaTroco = 0.5
+                            valorTroco += -0.5
+                            qntCentavosMaquina50 += -1
+
+                        elif valorTroco == 1 and qntNotaMaquina1 > 0:
+                            notaTroco = 1
+                            valorTroco += -1
+                            qntNotaMaquina1 += -1
+
+                        elif valorTroco == 2 and qntNotaMaquina2 > 0:
+                            notaTroco = 2
+                            valorTroco += -2
+                            qntNotaMaquina2 += -1
+
+                        # Verificação complexa
+                        elif 2 < valorTroco < 5 and qntNotaMaquina2 > 0:
+                            notaTroco = 2
+                            valorTroco += -2
+                            qntNotaMaquina2 += -1
+                        # Fim da Verificação complexa
+
+                        elif valorTroco == 5 and qntNotaMaquina5 > 0:
+                            notaTroco = 5
+                            valorTroco += -5
+                            qntNotaMaquina5 += -1
+
+                        elif valorTroco == 10 and qntNotaMaquina10 > 0:
+                            notaTroco = 10
+                            valorTroco += -10
+                            qntNotaMaquina10 += -1
+
+                        elif valorTroco == 20 and qntNotaMaquina20 > 0:
+                            notaTroco = 20
+                            valorTroco += -20
+                            qntNotaMaquina20 += -1
+
+                        elif valorTroco == 50 and qntNotaMaquina50 > 0:
+                            notaTroco = 50
+                            valorTroco += -50
+                            qntNotaMaquina50 += -1
+
+                        elif valorTroco == 100 and qntNotaMaquina100 > 0:
+                            notaTroco = 100
+                            valorTroco += -100
+                            qntNotaMaquina100 += -1
+
+                        else:
+                            print("\nNão possuo troco na máquina, não será possível realizar a compra :(")
+                            print("Devolvendo notas inseridas")
+                            # Reset notas inseridas
+                            notaTroco = 0
+                            valorTroco = 0
+                            valorTotalInserido = 0
+                            qntCentavos05 = 0
+                            qntCentavos10 = 0
+                            qntCentavos25 = 0
+                            qntCentavos50 = 0
+                            qntNota1 = 0
+                            qntNota2 = 0
+                            qntNota5 = 0
+                            qntNota10 = 0
+                            qntNota20 = 0
+                            qntNota50 = 0
+                            qntNota100 = 0
+
+                        if notaTroco > 0:
+                            # Devolver nota
+                            print("Devolvendo nota de R$ ", notaTroco)
+
+                # Contabilizar venda
+                if valorTroco == 0 and valorTotalInserido > 0:
+                    # Compra realizada
+                    print("Você adquiriu uma {} no valor de R$ {}".format(nameSelectedProduct, valorBebida))
+
+                    #Atualizando estoque
+                    if idProduct == 1:
+                        qntProduct1 += -1
+                    elif idProduct == 2:
+                        qntProduct2 += -1
+                    elif idProduct == 3:
+                        qntProduct3 += -1
+                    elif idProduct == 4:
+                        qntProduct4 += -1
+                    elif idProduct == 5:
+                        qntProduct5 += -1
+
+                    # Repassando notas para maquina
+                    qntCentavosMaquina05 += qntCentavos05
+                    qntCentavosMaquina10 += qntCentavos10
+                    qntCentavosMaquina25 += qntCentavos25
+                    qntCentavosMaquina50 += qntCentavos50
+                    qntNotaMaquina1 += qntNota1
+                    qntNotaMaquina2 += qntNota2
+                    qntNotaMaquina5 += qntNota5
+                    qntNotaMaquina10 += qntNota10
+                    qntNotaMaquina20 += qntNota20
+                    qntNotaMaquina50 += qntNota50
+                    qntNotaMaquina100 += qntNota100
+
+                    # Reset
+                    valorTotalInserido = 0
+                    qntCentavos05 = 0
+                    qntCentavos10 = 0
+                    qntCentavos25 = 0
+                    qntCentavos50 = 0
+                    qntNota1 = 0
+                    qntNota2 = 0
+                    qntNota5 = 0
+                    qntNota10 = 0
+                    qntNota20 = 0
+                    qntNota50 = 0
+                    qntNota100 = 0
+                    valorTotalMaquina = qntCentavosMaquina05 * 0.05 + qntCentavosMaquina10 * 0.1 + qntCentavosMaquina25 * 0.25 + \
+                                        qntCentavosMaquina50 * 0.50 + qntNotaMaquina1 * 1 + qntNotaMaquina2 * 2 + qntNotaMaquina5 * 5 + \
+                                        qntNota10 * 10 + qntNotaMaquina20 * 20 + qntNotaMaquina50 * 50 + qntNotaMaquina100 * 100
+
 
     else:
+        # Catch error
         print("Você digitou um produto inválido, por favor, digite novamente o produto desejado:")
