5e331ca1d129bd6c9908dc26d0ca9c1f50cf4e9a
Lucas H Polazzo <lucashpolazzo2013@gmail.com>
2019-04-02 12:45:16 -0300

    Correção do código para uso de while ao invés de funções

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+##### Variáveis #####
+
+isRunning = True
 idProduct = 0
 qntProduct1 = 10
 qntProduct2 = 10
@@ -16,10 +19,13 @@ qntNota10 = 0
 qntNota20 = 0
 qntNota50 = 0
 qntNota100 = 0
+valorTotal = qntCentavos5 * 0.05 + qntCentavos10 * 0.1 + qntCentavos25 * 0.25 + qntCentavos50 * 0.50 + qntNota1 * 1 + qntNota2 * 2 + qntNota5 * 5 + qntNota10 * 10 + qntNota20 * 20 + qntNota50 * 50 + qntNota100 * 100
 
+##### Início #####
 
-def SelectProduct():
-    global idProduct
+# Selecionar Bebida
+
+while isRunning:
     print("")
     print("-----------------------------------")
     print("Número|  Bebida          | Valor")
@@ -30,49 +36,46 @@ def SelectProduct():
     print("  5   |  Água mineral    | R$ 2,50")
     print("-----------------------------------")
     print("")
-    idProduct = int(input("Digite o número da bebida desejada: "))
+    idProduct = input("Digite o número da bebida desejada: \n")
 
-    VerifyIDProduct()
+    # Verificar se é numero
+    if idProduct.isnumeric():
+        idProduct = int(idProduct)
+    else:
+        idProduct = 404
 
-def AreaAdm():
-    print("Olá Administrador!")
-    print("A máquina possui o seguinte estoque:")
-    print()
-    print("-----------------------------------")
-    print("Número|  Bebida          | Quantidade")
-    print("  1   |  Coca-Cola       |   ", qntProduct1)
-    print("  2   |  Fanta Uva       |   ", qntProduct2)
-    print("  3   |  Fanta Laranja   |   ", qntProduct3)
-    print("  4   |  Sprite          |   ", qntProduct4)
-    print("  5   |  Água mineral    |   ", qntProduct5)
-    print("-----------------------------------")
-    print()
-    print("A máquina está com as seguintes notas:")
-    print()
-    print("-----------------------------------")
-    print("Nota R$ | Quantidade | R$ Total")
-    print("  0,05  |     {}     |    {:.2f}".format(qntCentavos5, qntCentavos5 * 0.05))
-    print("  0,10  |     {}     |    {:.2f}".format(qntCentavos10, qntCentavos10 * 0.1))
-    print("  0,25  |     {}     |    {:.2f}".format(qntCentavos25, qntCentavos25 * 0.25))
-    print("  0,50  |     {}     |    {:.2f}".format(qntCentavos50, qntCentavos50 * 0.50))
-    print("  1,00  |     {}     |    {:.2f}".format(qntNota1, qntNota1 * 1))
-    print("  2,00  |     {}     |    {:.2f}".format(qntNota2, qntNota2 * 2))
-    print("  5,00  |     {}     |    {:.2f}".format(qntNota5, qntNota5 * 5))
-    print("  10,00 |     {}     |    {:.2f}".format(qntNota10, qntNota10 * 10))
-    print("  20,00 |     {}     |    {:.2f}".format(qntNota20, qntNota20 * 20))
-    print("  50,00 |     {}     |    {:.2f}".format(qntNota50, qntNota50 * 50))
-    print("  100,00|     {}     |    {:.2f}".format(qntNota100, qntNota100 * 100))
-    print("-----------------------------------")
-    print("A máquina possui R$ {:.2f}".format(qntCentavos5 * 0.05 +
-          qntCentavos10 * 0.1 + qntCentavos25 * 0.25 + qntCentavos50 * 0.50 +
-          qntNota1 * 1 + qntNota2 * 2 + qntNota5 * 5 + qntNota10 * 10 +
-          qntNota20 * 20 + qntNota50 * 50 + qntNota100 * 100))
-    print()
-
-def VerifyIDProduct():
-    if idProduct == 996:
+    if idProduct == 996: # 996 = admPassword
         # Área do admnistrador
-        AreaAdm()
+        print("Olá Administrador!")
+        print("A máquina possui o seguinte estoque:")
+        print()
+        print("-----------------------------------")
+        print("Número|  Bebida          | Quantidade")
+        print("  1   |  Coca-Cola       |   ", qntProduct1)
+        print("  2   |  Fanta Uva       |   ", qntProduct2)
+        print("  3   |  Fanta Laranja   |   ", qntProduct3)
+        print("  4   |  Sprite          |   ", qntProduct4)
+        print("  5   |  Água mineral    |   ", qntProduct5)
+        print("-----------------------------------")
+        print()
+        print("A máquina está com as seguintes notas:")
+        print()
+        print("-----------------------------------")
+        print("Nota R$ | Quantidade | R$ Total")
+        print("  0,05  |     {}     |    {:.2f}".format(qntCentavos5, qntCentavos5 * 0.05))
+        print("  0,10  |     {}     |    {:.2f}".format(qntCentavos10, qntCentavos10 * 0.1))
+        print("  0,25  |     {}     |    {:.2f}".format(qntCentavos25, qntCentavos25 * 0.25))
+        print("  0,50  |     {}     |    {:.2f}".format(qntCentavos50, qntCentavos50 * 0.50))
+        print("  1,00  |     {}     |    {:.2f}".format(qntNota1, qntNota1 * 1))
+        print("  2,00  |     {}     |    {:.2f}".format(qntNota2, qntNota2 * 2))
+        print("  5,00  |     {}     |    {:.2f}".format(qntNota5, qntNota5 * 5))
+        print("  10,00 |     {}     |    {:.2f}".format(qntNota10, qntNota10 * 10))
+        print("  20,00 |     {}     |    {:.2f}".format(qntNota20, qntNota20 * 20))
+        print("  50,00 |     {}     |    {:.2f}".format(qntNota50, qntNota50 * 50))
+        print("  100,00|     {}     |    {:.2f}".format(qntNota100, qntNota100 * 100))
+        print("-----------------------------------")
+        print("A máquina possui R$ {:.2f}".format(valorTotal))
+        print()
     elif 0 < idProduct <= 5:
         # Converção de ID para String dos produtos
         if idProduct == 1:
@@ -87,17 +90,12 @@ def VerifyIDProduct():
             selectedProduct = "Água Mineral"
 
         # Verificação bebida
-        print("Você selecionou a bebida de {}, está correto ou deseja alterar?: (S/N)".format(selectedProduct))
+        print("Você selecionou a bebida de {}, está correto?: (S/N)".format(selectedProduct))
         simOuNao = input()
 
         if simOuNao == "s" or simOuNao == "S":
             print("Beleza")
-        else:
-            SelectProduct()
+            isRunning = False
 
     else:
         print("Você digitou um produto inválido, por favor, digite novamente o produto desejado:")
-        SelectProduct()
-
-
-SelectProduct()
